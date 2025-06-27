5ddef6911e0e9e73209e0fd7af1fe327f337fded
portbound <prtbnd@proton.me>
2025-06-26 21:19:17 -0400

    1. TwoSum (C#) - fixed formatting

--- easy/lc1.cs
+++ easy/lc1.cs
@@ -1,10 +1,10 @@
 public int[] TwoSum(int[] nums, int target){
-		for (int i = 0; i < nums.Length; i++){
-				for (int j = i + 1; j < nums.Length; j++){
-						if (nums[i] + nums[j] == target) {
-								return new int[] {i, j};
-						}
-				}
+	for (int i = 0; i < nums.Length; i++){
+		for (int j = i + 1; j < nums.Length; j++){
+			if (nums[i] + nums[j] == target) {
+					return new int[] {i, j};
+			}
 		}
-		return new int[0];
+	}
+	return new int[0];
 }
